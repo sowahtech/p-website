ea1eafbe48befe65c731aaa21b04f7ff8d9408e1
sowahtech <sowahtech@gmail.com>
2022-11-02 22:51:34 +0000

    logo added to header section

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="header-left">
+        <h2>brainy-dev</h2>
+      </div>
+      <div className="header-right"></div>
+    </div>
+  );
 };
 
 export default Header;
