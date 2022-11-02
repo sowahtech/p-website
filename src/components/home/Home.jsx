3671c04f8bb6a02487f46bc33baf6de3a87011d8
sowahtech <sowahtech@gmail.com>
2022-11-02 22:22:58 +0000

    package-lock.json deleted, App.js modified

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1 className="test-header">header component</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
